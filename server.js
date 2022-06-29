7cc76893483f6eae6f8d9250e1ea3b949f5ba182
GSiggins <gregory.siggins@gmail.com>
2022-06-29 09:56:46 -0700

    fixed port syntax

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require ('express');
 const path = require ('path');
 const api = require('./routes/api')
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.static('public'));
